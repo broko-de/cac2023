08122301ccd09b8f472f56faae26af1bbd60b62f
Fede Liquin <josefliquin@gmail.com>
2023-06-15 08:56:37 -0300

    Se modifica settings.py para aceptar mysql

--- cac2023/settings.py
+++ cac2023/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'cac2023.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': env("DATABASE_NAME"),
         'USER': env("DATABASE_USER"),
         'PASSWORD': env("DATABASE_PASSWORD"),
